04829f0b44502b0b4b06684dae68189c41a7ca98
ShemK <shemk@vt.edu>
2017-07-06 09:18:24 -0400

    Adding Log Files

---
+++ logs/octave/scenario_master_template_summary.m
@@ -0,0 +1,9 @@
+scenario_name{1} = 'basic_two_node_network';
+run_time(1) = 30;
+num_nodes(1) = 2;
+
+bytes_sent(1,1,1) = 0;
+bytes_received(1,1,1) = 0;
+bytes_sent(1,2,1) = 0;
+bytes_received(1,2,1) = 0;
+
